05251b61045fb6492f0b0177b937567dabfbe828
Jailton Silva <jss1@ifal.edu.br>
2024-05-16 13:35:25 -0300

    fix: closing the scanner

--- solutions/src/introduction/JavaStdInAndStdOut.java
+++ solutions/src/introduction/JavaStdInAndStdOut.java
@@ -9,6 +9,7 @@ public class JavaStdInAndStdOut {
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
+        scan.close();
 
         System.out.println(a);
         System.out.println(b);
