61995e22f575f2327d39a46e9f4c835a4d9ac788
Jailton Silva <jss1@ifal.edu.br>
2024-05-18 13:39:53 -0300

    add a solution for the problem Inheritance 1

---
+++ solutions/src/oop/Inheritance1.java
@@ -0,0 +1,38 @@
+/*
+Task:
+Implement a sing method in the class Bird
+
+Expected Output:
+I am walking
+I am flying
+I am singing
+ */
+package oop;
+
+
+class Animal {
+    void walk() {
+        System.out.println("I am walking");
+    }
+}
+
+
+class Bird extends Animal {
+    void fly(){
+        System.out.println("I am flying");
+    }
+
+    public void sing() {
+        System.out.println("I am singing");
+    }
+}
+
+
+public class Inheritance1 {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
