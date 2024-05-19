38d0c53bcd319936c4c13bf3a6a819b6002e1ea0
Jailton Silva <jss1@ifal.edu.br>
2024-05-18 23:26:12 -0300

    fix: solving error can't find main(String[]) method in class: oop.Animal

--- solutions/src/oop/Inheritance1.java
+++ solutions/src/oop/Inheritance1.java
@@ -10,6 +10,16 @@ I am singing
 package oop;
 
 
+public class Inheritance1 {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
+
+
 class Animal {
     void walk() {
         System.out.println("I am walking");
@@ -24,15 +34,5 @@ class Bird extends Animal {
 
     public void sing() {
         System.out.println("I am singing");
-    }
-}
-
-
-public class Inheritance1 {
-    public static void main(String[] args) {
-        Bird bird = new Bird();
-        bird.walk();
-        bird.fly();
-        bird.sing();
     }
 }
