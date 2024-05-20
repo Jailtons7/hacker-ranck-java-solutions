db7132a709b682e9aa4f209f8d5139236cc436cf
Jailton Silva <jss1@ifal.edu.br>
2024-05-19 22:47:53 -0300

    fix: solving error can't find main(String[]) method in class: oop.Interface

--- solutions/src/oop/Interface.java
+++ solutions/src/oop/Interface.java
@@ -17,24 +17,6 @@ package oop;
 import java.util.*;
 
 
-interface AdvancedArithmetic {
-    int divisor_sum(int n);
-}
-
-
-class MyCalculator implements AdvancedArithmetic {
-    public int divisor_sum(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                sum += i;
-            }
-        }
-        return sum;
-    }
-}
-
-
 class Interface {
     public static void main(String []args){
         MyCalculator my_calculator = new MyCalculator();
@@ -54,5 +36,23 @@ class Interface {
             String interfaceName = theInterfaces[i].getName();
             System.out.println(interfaceName);
         }
+    }
+}
+
+
+interface AdvancedArithmetic {
+    int divisor_sum(int n);
+}
+
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
